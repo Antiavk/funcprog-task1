d6f728854554447ff52b022ba3fa9b3fa19e4d75
Antiavk <avk_tp@mail.ru>
2022-08-11 11:02:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //реализатор Function
+        Function<String, String[]> stringToDictionaryArray = (string) -> {
+            return Arrays.stream(string.split(" "))
+                    .sorted()
+                    .distinct()
+                    .toArray(String[]::new);
+        };
+
+        //Использование Function
+        String[] dictionary = stringToDictionaryArray.apply(
+                new Scanner(System.in).nextLine()
+        );
+
+        //Применение Stream API
+        Arrays.stream(dictionary)
+                .forEach(System.out::println);
+    }
+}
